ea84c1b362364bf7e40916779b187a2f98690f1d
Shane <shanekoppers@hotmail.com>
2014-11-24 17:47:41 +0100

    Added a method die calculate the points of a team

--- com/louislife/model/Game.java
+++ com/louislife/model/Game.java
@@ -3,15 +3,31 @@ package com.louislife.model;
 import java.util.ArrayList;
 
 /**
- * Represents a Game object with leagues and played matches. This holds all the game data necessary for one instance of the game to function.
+ * Represents a Game object with leagues and played matches. This holds all the
+ * game data necessary for one instance of the game to function.
+ * 
  * @author hidde
  *
  */
 public class Game {
-	
+
 	private int id;
 	private String name;
 	private ArrayList<League> leagues;
 	private ArrayList<Match> matches;
-	
+
+	public int calcPoints(Team team) {
+		int points = 0;
+		for (int i = 0; i < matches.size(); i++) {
+			if (team.equals(matches.get(i).getWinningTeam())) {
+				points = points + 3;
+			}
+			if (team.equals(matches.get(i).getTeamAway())
+					|| team.equals(matches.get(i).getTeamHome())
+					&& matches.get(i).getResult() == MatchResult.TIE) {
+				points++;
+			}
+		}
+		return points;
+	}
 }
