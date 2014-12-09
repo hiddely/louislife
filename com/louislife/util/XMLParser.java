6b475e8b45b9084a4c80ca40f24599cc9259ff0a
Hidde <mail.hidde@gmail.com>
2014-12-09 16:56:27 +0100

    Some commit

--- com/louislife/util/XMLParser.java
+++ com/louislife/util/XMLParser.java
@@ -1,11 +1,18 @@
 package com.louislife.util;
 
+import java.io.File;
 import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
@@ -187,6 +194,82 @@ public class XMLParser {
 		}
 				
 		return game;
+	}
+	
+	public boolean writeGame(Game g) {
+		try {
+			 
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+	 
+			// root elements
+			Document doc = docBuilder.newDocument();
+			Element rootElement = doc.createElement("game");
+			doc.appendChild(rootElement);
+			
+			rootElement.setAttribute("id", g.getId()+"");
+			rootElement.setAttribute("name", g.getName()+"");
+			rootElement.setAttribute("currentday", g.getCurrentDay()+"");
+			rootElement.setAttribute("currentteam", g.getUserTeam().getId()+"");
+			
+			for (Team t : g.getLeagues().get(0).getTeams()) {
+				
+			}
+			
+			
+			// staff elements
+			Element staff = doc.createElement("Staff");
+			rootElement.appendChild(staff);
+	 
+			// set attribute to staff element
+			Attr attr = doc.createAttribute("id");
+			attr.setValue("1");
+			staff.setAttributeNode(attr);
+	 
+			// shorten way
+			// staff.setAttribute("id", "1");
+	 
+			// firstname elements
+			Element firstname = doc.createElement("firstname");
+			firstname.appendChild(doc.createTextNode("yong"));
+			staff.appendChild(firstname);
+	 
+			// lastname elements
+			Element lastname = doc.createElement("lastname");
+			lastname.appendChild(doc.createTextNode("mook kim"));
+			staff.appendChild(lastname);
+	 
+			// nickname elements
+			Element nickname = doc.createElement("nickname");
+			nickname.appendChild(doc.createTextNode("mkyong"));
+			staff.appendChild(nickname);
+	 
+			// salary elements
+			Element salary = doc.createElement("salary");
+			salary.appendChild(doc.createTextNode("100000"));
+			staff.appendChild(salary);
+	 
+			// write the content into xml file
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File(filename));
+	 
+			// Output to console for testing
+			// StreamResult result = new StreamResult(System.out);
+	 
+			transformer.transform(source, result);
+	 
+			System.out.println("File saved!");
+	 
+			return true;
+		  } catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+		  } catch (TransformerException tfe) {
+			tfe.printStackTrace();
+		  }
+		}
+	return false;
 	}
 	
 	public String getChildValue(Node p, String name) {
