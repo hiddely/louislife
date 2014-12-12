6ce27745bd34c1967b89c8aba9af9da6740ba5ef
Wouter Smit <pathemeous@gmail.com>
2014-12-12 15:01:54 +0100

    Comments

--- com/louislife/controller/OverviewController.java
+++ com/louislife/controller/OverviewController.java
@@ -2,6 +2,15 @@ package com.louislife.controller;
 
 import java.util.Observable;
 
+/**
+ * This controller handles all events that happen inside the Overview screen.
+ * This is the main screen from which the user can access all game features,
+ * such as playing the next match, going to team management, etc.
+ * 
+ * @author Pathemeous
+ *
+ */
+
 public class OverviewController extends Observable {
 
 }
