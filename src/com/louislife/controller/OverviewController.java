b98eedc9404a2ee70fa34306e2e1fefc6d6055a9
Wouter Smit <pathemeous@gmail.com>
2014-12-15 16:30:29 +0100

    Added OverviewController with appropriate comments (Methods still empty)

---
+++ src/com/louislife/controller/OverviewController.java
@@ -0,0 +1,20 @@
+package com.louislife.controller;
+
+import java.util.Observable;
+
+/**
+ * Controller Class to control the events in the Overview screen (main game window).
+ * @author Pathemeous
+ *
+ */
+
+public class OverviewController extends Observable {
+	
+	
+	/**
+	 * This method must update the model to reflect one match being played and bring a feedback in the view with the results of this match.
+	 */
+	public void nextMatch() {
+		//TODO: Add the Controller method for SpelGenerator
+	}
+}
