2d1a9d0dde5fff8422d0f3cb60e170f0c91ef9ee
Elgar <eweijtmans@hotmail.com>
2014-12-21 16:42:22 +0100

    Add tests package

---
+++ src/com/louislife/tests/PlayerTest.java
@@ -0,0 +1,135 @@
+package com.louislife.tests;
+import com.louislife.model.Player;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Test
+	public void testPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFirstname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFirstname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSurname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSurname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetJerseyNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetJerseyNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOffensiveScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOffensiveScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDefensiveScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDefensiveScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStaminaScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetStaminaScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTeamId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTeamId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsPlayer() {
+		fail("Not yet implemented");
+	}
+
+}
