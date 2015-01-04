bd5265ecc5c4a1f08458b776d004691106362f52
Hidde <mail.hidde@gmail.com>
2015-01-04 17:49:43 +0100

    Add team formation editor start Add team list

--- src/com/louislife/controller/TeamController.java
+++ src/com/louislife/controller/TeamController.java
@@ -1,19 +1,27 @@
 package com.louislife.controller;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 
 import com.louislife.UI.ControlledScreen;
 import com.louislife.UI.ScreensController;
+import com.louislife.model.Game;
+import com.louislife.model.Player;
 
 public class TeamController implements Initializable,
 ControlledScreen {
 
 	ScreensController controller;
-
+	
 	/** XML Properties **/
+	@FXML private ListView<String> playerList;
 	
 	@Override
 	public void setScreenParent(ScreensController screenParent) {
@@ -23,6 +31,17 @@ ControlledScreen {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
+		ArrayList<Player> players = Game.getInstance().getUserTeam().getPlayers();
+		String[] playerListDisplay = new String[players.size()];
+		
+		for (int i = 0; i < players.size(); i++) {
+			Player p = players.get(i);
+			playerListDisplay[i] = p.getJerseyNumber() + " - " + p.getFirstname() + " " + p.getSurname();
+		}
+		
+		// En zet ze dynamisch in de UI
+		ObservableList<String> items =FXCollections.observableArrayList(playerListDisplay);
+		playerList.setItems(items);
 
 	}
 }
