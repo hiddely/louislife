43e587730598642126b7b851d0055a71aac85fdd
Shane <shanekoppers@hotmail.com>
2015-01-22 21:28:46 +0100

    Added UML

--- src/com/louislife/model/Match.java
+++ src/com/louislife/model/Match.java
@@ -192,7 +192,6 @@ public class Match {
 		
 		int homeChances = home.getTotStamina() / 110;
 		int awayChances = away.getTotStamina() / 120;
-		System.out.println(homeChances + " / "+ awayChances);
 		double homeGoalChance = (50 + ((home.getTotOff() * 2 - away.getTotDef()) * 100 / away.getTotDef()));
 		double awayGoalChance = (50 + ((away.getTotOff() * 2 - home.getTotDef()) *100 / home.getTotDef()));
 		if(homeGoalChance < 5){
@@ -205,8 +204,6 @@ public class Match {
 		}else if(awayGoalChance > 90){
 			awayGoalChance = 90;
 		}
-		System.out.println("50 + " + home.getTotOff() + " - " + away.getTotDef() + " * 100 / " + away.getTotDef());
-		System.out.println(homeGoalChance + " / "+ awayGoalChance);
 
 		int homeTime = 0;
 		int awayTime = 0;
